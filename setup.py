cc7ab96af50ef2ef16656322734bb2f5045a7c4f
Trevor Manz <trevor.j.manz@gmail.com>
2020-02-13 13:36:27 -0500

    add add requirements.txt

--- setup.py
+++ setup.py
@@ -1,44 +1,80 @@
 #!/usr/bin/env python
-
-"""The setup script."""
-
+import io
+import os
+import re
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
-    readme = readme_file.read()
 
-requirements = ['Click>=7.0', ]
+def read(path, encoding="utf-8"):
+    path = os.path.join(os.path.dirname(__file__), path)
+    with io.open(path, encoding=encoding) as fp:
+        return fp.read()
+
+
+def get_install_requirements(path):
+    content = read(path)
+    return [
+        requirement
+        for requirement in content.split("\n")
+        if requirement != "" and not requirement.startswith("#")
+    ]
+
+
+def version(path):
+    """Obtain the package version from a python file e.g. pkg/__init__.py
+    See <https://packaging.python.org/en/latest/single_source_version.html>.
+    """
+    version_file = read(path)
+    version_match = re.search(
+        r"""^__version__ = ['"]([^'"]*)['"]""", version_file, re.M
+    )
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+DESCRIPTION = "Python package for converting images to zarr chunked arrays."
+LONG_DESCRIPTION = read("README.md")
+LONG_DESCRIPTION_CONTENT_TYPE = "text/markdown"
+NAME = "img2zarr"
+PACKAGES = find_packages(include=[NAME, f"{NAME}.*"])
+AUTHOR = "Heath Patterson / Trevor Manz"
+AUTHOR_EMAIL = "trevor.j.manz@gmail.com"
+URL = "https://img2zarr.readthedocs.io."
+DOWNLOAD_URL = "https://github.com/hubmapconsortium/img2zarr"
+LICENSE = "MIT license"
+INSTALL_REQUIRES = get_install_requirements("requirements.txt")
+PYTHON_REQUIRES = ">=3.5"
+DEV_REQUIRES = get_install_requirements("requirements_dev.txt")
+VERSION = "0.0.0"
+
 
 setup(
-    author="Trevor James Manz",
-    author_email='trevor.j.manz@gmail.com',
-    python_requires='>=3.5',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
-    description="Python package for converting images to zarr chunked arrays.",
-    entry_points={
-        'console_scripts': [
-            'img2zarr=img2zarr.cli:main',
-        ],
-    },
-    install_requires=requirements,
-    license="MIT license",
-    long_description=readme,
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESCRIPTION_CONTENT_TYPE,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    download_url=DOWNLOAD_URL,
+    license=LICENSE,
+    packages=PACKAGES,
     include_package_data=True,
-    keywords='img2zarr',
-    name='img2zarr',
-    packages=find_packages(include=['img2zarr', 'img2zarr.*']),
-    test_suite='tests',
-    url='https://github.com/hubmapconsortium/img2zarr',
-    version='0.0.0',
+    install_requires=INSTALL_REQUIRES,
+    python_requires=PYTHON_REQUIRES,
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    entry_points={"console_scripts": ["img2zarr=img2zarr.cli:main"]},
     zip_safe=False,
 )
