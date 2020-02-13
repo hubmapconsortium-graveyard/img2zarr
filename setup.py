323ff21ec1c54cbbe493de6d812eba42291ae633
Trevor Manz <trevor.j.manz@gmail.com>
2020-02-13 13:12:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,8 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open('README.md') as readme_file:
     readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
 
 requirements = ['Click>=7.0', ]
 
@@ -35,7 +32,7 @@ setup(
     },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='img2zarr',
     name='img2zarr',
